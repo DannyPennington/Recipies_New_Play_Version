e4aed7a9b4d76bc71c78825f71b458a4d8e2eb8b
Danny <dannypen@gmail.com>
2020-03-13 14:26:04 +0000

    Initial commit

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,7 +7,12 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Hello World!"))
   }
 
+  def help = Action {
+    Redirect("https://www.google.com")
+  }
+
+  def todo = TODO
 }
