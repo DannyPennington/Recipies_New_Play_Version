e42efbd8f7f9518c3ad4b7d0da9541ce31db01be
Danny <dannypen@gmail.com>
2020-03-13 15:20:27 +0000

    Added explicit return types to controllers to fix warnings

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,20 +6,20 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
+  def index: Action[AnyContent] = Action {
     Ok(views.html.index("Hello World!")).as(HTML).withCookies(
       Cookie("colour","blue")
     )
   }
-  def index2 = Action {
+  def index2: Action[AnyContent] = Action {
     Ok(views.html.index("Index2 page here")).as("text/html")
   }
 
-  def help = Action {
+  def help: Action[AnyContent] = Action {
     Redirect("https://www.google.com")
   }
 
-  def cookiePage = Action { implicit request: Request[AnyContent] =>
+  def cookiePage: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     if (request.cookies.get("colour").isDefined) {
       Ok(request.cookies.get("colour"))
     }
@@ -28,5 +28,5 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     }
   }
 
-  def todo = TODO
+  def todo: Action[AnyContent] = TODO
 }
