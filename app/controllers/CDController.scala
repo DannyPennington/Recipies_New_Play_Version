c1431698c89263db4f291794452864e9c82c44cd
Danny <dannypen@gmail.com>
2020-03-16 17:12:35 +0000

    Renamed CD controller to make more readable

--- app/controllers/CDController.scala
+++ app/controllers/CDController.scala
@@ -4,13 +4,13 @@ import javax.inject.Inject
 import models.CD
 import play.api.mvc._
 
-class CD @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport {
+class CDController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport {
 
   def showCDForm:Action[AnyContent] = Action { implicit request:Request[AnyContent] =>
     Ok(views.html.cd(CD.createCDForm))
   }
 
-  def submitCD = Action { implicit request:Request[AnyContent] =>
+  def submitCD: Action[AnyContent] = Action { implicit request:Request[AnyContent] =>
     CD.createCDForm.bindFromRequest.fold({ formWithErrors =>
       BadRequest(views.html.cd(formWithErrors))
     }, { cd =>
--- app/models/CD.scala
+++ app/models/CD.scala
@@ -3,15 +3,15 @@ package models
 import play.api.data.Form
 import play.api.data.Forms._
 
-case class CD(name: String, price: Int) {
-  override def toString() = s"The name of the CD is: $name and the price is $price"
+case class CD(name: String, price: BigDecimal) {
+  override def toString = s"The name of the CD is: $name and the price is £$price"
 }
 
 object CD {
-  val createCDForm = Form(
+  val createCDForm: Form[CD]= Form(
     mapping(
       "name" -> nonEmptyText,
-      "price" -> number
+      "price" -> bigDecimal
     )(CD.apply)(CD.unapply)
   )
 }
