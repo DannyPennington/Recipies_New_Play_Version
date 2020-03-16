40e08e6837e5113988256a0545a27ddbbe8f1904
Danny <dannypen@gmail.com>
2020-03-16 17:04:14 +0000

    CD form setup to take input for CD object and displays it on next page with GET method

---
+++ app/controllers/CD.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import javax.inject.Inject
+import models.CD
+import play.api.mvc._
+
+class CD @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport {
+
+  def showCDForm:Action[AnyContent] = Action { implicit request:Request[AnyContent] =>
+    Ok(views.html.cd(CD.createCDForm))
+  }
+
+  def submitCD = Action { implicit request:Request[AnyContent] =>
+    CD.createCDForm.bindFromRequest.fold({ formWithErrors =>
+      BadRequest(views.html.cd(formWithErrors))
+    }, { cd =>
+      Ok(cd.toString())
+    })
+  }
+
+}
